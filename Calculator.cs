6676f306e702866e61fe16e55bb140e82cbe4405
Zainal5172 <zainal000789@students.amikom.ac.id>
2023-03-16 14:55:19 +0700

    menambahkan perkalian

--- Calculator.cs
+++ Calculator.cs
@@ -6,7 +6,10 @@ class Calculator
 		int b = 6;
 		
 		Console.WriteLine("Hasil penambahan : {0} + {1} = {2}", a, b, Penambahan(a, b));
-		Console.WriteLine("Hasil pengurangan: {0} + {1} = {2}", a, b, Pengurangan(a, b));
+		Console.WriteLine("Hasil pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		Console.WriteLine("Hasil perkalian  : {0} * {1} = {2}", a, b, Perkalian(a, b));
+		
+		
 		
 		Console.WriteLine("\nTekan sembarang key untuk keluar");
 		Console.Readkey();
@@ -18,5 +21,9 @@ class Calculator
 	static int Pengurangan(int a, int b);
 	{
 		return a - b;
+	}
+	static int Perkalian(int a, int b);
+	{
+		return a * b;
 	}
 }
